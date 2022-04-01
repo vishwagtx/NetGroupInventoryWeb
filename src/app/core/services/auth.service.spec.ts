ac44d758cb032da038064417864b97a459e57e9f
Vishwa Weerakkody <vishwais@hotmail.com>
2022-04-01 21:15:51 +0530

    Started write few unit test cases

--- src/app/core/services/auth.service.spec.ts
+++ src/app/core/services/auth.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
+import { User, UserSettings } from 'oidc-client';
 
 import { AuthService } from './auth.service';
 
@@ -16,5 +17,20 @@ describe('AuthService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('signInUser', () => {
+    it('should be return passed user', () => {
+      const settings = {
+        access_token: 'tttttttttttttttt',
+      } as UserSettings;
+
+      const user = new User(settings);
+      service.signInUser(user);
+
+      service.signInPostActions$().subscribe((u: User) => {
+        expect(u.access_token).toEqual(user.access_token);
+      });
+    });
   });
 });
--- src/app/inventory/items/form-dialog/item-form-dialog.component.spec.ts
+++ src/app/inventory/items/form-dialog/item-form-dialog.component.spec.ts
@@ -1,26 +1,34 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { of } from 'rxjs';
 import { UploadService } from 'src/app/core/services/upload.service';
 import { ItemsService } from '../../services/items.service';
-
+import { IReponse } from 'src/app/shared/models/response';
 import { ItemFormDialogComponent } from './item-form-dialog.component';
+import { IItem } from '../../models/item';
 
 describe('ItemFormDialogComponent', () => {
   let component: ItemFormDialogComponent;
   let fixture: ComponentFixture<ItemFormDialogComponent>;
+  let matDialogRefSpy = jasmine.createSpyObj({
+    close: () => true,
+  });
+  let itemsService: ItemsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
       providers: [
-        { provide: MatDialogRef, useValue: {} },
-        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: matDialogRefSpy },
+        { provide: MAT_DIALOG_DATA, useValue: null },
         ItemsService,
         UploadService,
       ],
       declarations: [ItemFormDialogComponent],
     }).compileComponents();
+
+    itemsService = TestBed.inject(ItemsService);
   });
 
   beforeEach(() => {
@@ -31,5 +39,32 @@ describe('ItemFormDialogComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('save ', () => {
+    beforeEach(() => {
+      component.itemForm.controls.title.setValue('Test');
+      component.itemForm.controls.categoryId.setValue(1);
+    });
+
+    it('should be called create function when data is null', () => {
+      const spy = spyOn(itemsService, 'create');
+      spy.and.returnValue(of({ succeed: true } as IReponse<number>));
+
+      component.save({ preventDefault: () => {} } as Event);
+
+      expect(spy).toHaveBeenCalled();
+    });
+
+    it('should be called update function when data is not null', () => {
+      component.data = { id: 1 } as IItem;
+
+      const spy = spyOn(itemsService, 'update');
+      spy.and.returnValue(of({ succeed: true } as IReponse<number>));
+
+      component.save({ preventDefault: () => {} } as Event);
+
+      expect(spy).toHaveBeenCalled();
+    });
   });
 });
