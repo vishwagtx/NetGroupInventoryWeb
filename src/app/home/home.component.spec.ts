5cc8dd6ca3e4bcacd74c3b9da984e010ac16d061
Vishwa Weerakkody <vishwais@hotmail.com>
2022-04-02 18:57:55 +0530

    Fixed the unit test failed

--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,5 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AuthService } from '../core/services/auth.service';
 
 import { HomeComponent } from './home.component';
@@ -10,7 +10,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
+      imports: [RouterTestingModule],
       providers: [AuthService],
       declarations: [HomeComponent],
     }).compileComponents();
