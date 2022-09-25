ea7b9619578ba81737faa08263b59edb59958cf3
Vishwa Weerakkody <vishwa@calcey.com>
2022-09-25 09:15:34 +0530

    changed env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  appURL: 'https://polite-river-0cb038910.1.azurestaticapps.net',
+  openId: {
+    client_id: 'i_spa',
+    authority: 'https://selfstoragemanageidentity.azurewebsites.net/',
+  },
+  apiURL: 'https://selfstoragemanageapi.azurewebsites.net/',
 };
