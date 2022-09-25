1a9f1837c488d43080808cf160732306857aa710
Vishwa Weerakkody <vishwa@calcey.com>
2022-09-25 10:19:20 +0530

    Changed app url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  appURL: 'https://polite-river-0cb038910.1.azurestaticapps.net',
+  appURL: 'https://selfstoragemanageidentity.azurewebsites.net',
   openId: {
     client_id: 'i_spa',
     authority: 'https://selfstoragemanageidentity.azurewebsites.net/',
